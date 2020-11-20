66fa1174875c41279e21e5d0ca2dda0b3ed01927
Oskar Kupski <oskar.kupski@gmail.com>
2020-11-20 12:11:11 +0100

    Added amplify auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import {withAuthenticator} from "@aws-amplify/ui-react";
 
 const App = () => <div className="App">App</div>;
 
-export default App;
+export default withAuthenticator(App);
